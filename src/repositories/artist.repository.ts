173b3fe81f2fa56dc48a0b1e56f5f6f9368f6820
Pedro Fernandes Barreto Costa <fernandespedrobc@gmail.com>
2023-08-13 13:17:08 -0300

    FEAT: Artist model, respository, entity and service

---
+++ src/repositories/artist.repository.ts
@@ -0,0 +1,33 @@
+import ArtistsEntity from "../entities/artist.entity";
+import BaseRepository from "./base.repository";
+
+class ArtistRepository extends BaseRepository<ArtistsEntity> {
+  constructor() {
+    super("tests");
+  }
+
+  public async getArtists(): Promise<ArtistsEntity[]> {
+    return await this.findAll();
+  }
+
+  public async getArtist(id: string): Promise<ArtistsEntity | null> {
+    return await this.findOne((item) => item.id === id);
+  }
+
+  public async createArtist(data: ArtistsEntity): Promise<ArtistsEntity> {
+    return await this.add(data);
+  }
+
+  public async updateArtist(
+    id: string,
+    data: ArtistsEntity
+  ): Promise<ArtistsEntity | null> {
+    return await this.update((item) => item.id === id, data);
+  }
+
+  public async deleteArtist(id: string): Promise<void> {
+    await this.delete((item) => item.id !== id);
+  }
+}
+
+export default ArtistRepository;
