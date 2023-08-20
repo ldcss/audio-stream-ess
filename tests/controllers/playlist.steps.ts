39b592c711547787f08dba9ed182b5e9949955b3
ldcss <ldcs@cin.ufpe.br>
2023-08-20 17:51:15 -0300

    <fix> fix playlists tests

--- tests/controllers/playlist.steps.ts
+++ tests/controllers/playlist.steps.ts
@@ -1,11 +1,8 @@
 import { loadFeature, defineFeature } from "jest-cucumber"
-import supertest from "supertest";
-import app from "../../src/app"
 import PlaylistEntity from "../../src/entities/playlist.entity";
 import PlaylistRepository from "../../src/repositories/playlist.repository";
 
 const feature = loadFeature('tests/features/criar_categorias_playlists.feature')
-const request = supertest(app)
 
 jest.mock("../../src/repositories/playlist.repository")
 
@@ -22,51 +19,58 @@ defineFeature(feature, (test) => {
       name: "melhores do grime",
       genre: "grime",
       description: "",
-      idUser: 0
+      idUser: 0,
+      duration: 25
     }, {
       id: "2",
       name: "melhores do mpb",
       genre: "mpb",
       description: "",
-      idUser: 0
+      idUser: 0,
+      duration: 50
     }, {
       id: "3",
       name: "UK Drill",
       genre: "drill",
       description: "",
-      idUser: 0
+      idUser: 0,
+      duration: 120
     }]
   });
   
   test('Playlist sem categoria', ({given, when, then, and}) => {
     given(/^que eu sou um usuário logado no sistema com o id "(.*)"$/, async (id)=>{
       //Checagem com login
+      if(id==='0')
         mockGetPlaylist.mockReturnValue(mockPlaylists)
     });
 
     when(/^uma requisição GET for enviada para "(.*)"$/,
     async (url)=>{
       if(url){
-        const response = mockGetPlaylist(); //lpaylists do user com id 0
+        const response = mockGetPlaylist(); //playlists do user com id 0
         expect(response).toEqual([
         {
           id: '1',
           name: 'melhores do grime',
           genre: 'grime',
           description: '',
-          idUser: 0
+          idUser: 0,
+          duration: 25
         }, {
           id: '2',
           name: 'melhores do mpb',
           genre: 'mpb',
           description: '',
-          idUser: 0
+          idUser: 0,
+          duration: 50
         }, {
           id: '3',
           name: 'UK Drill',
           genre: 'drill',
           description: '',
-          idUser: 0
+          idUser: 0,
+          duration: 120
         }
         ])}
     });
@@ -82,4 +86,86 @@ defineFeature(feature, (test) => {
 			expect(+responseStatusCode).toBe(statusCode)
     })
 	})
+
+  test('Playlists por gênero', ({given, when, then, and}) => {
+    given(/^que eu sou um usuário logado no sistema com o id "(.*)"$/, async (id)=>{
+      //Checagem com login
+        mockGetPlaylist.mockReturnValue(mockPlaylists)
+    });
+
+    when(/^uma requisição GET for enviada para "(.*)"$/,
+    async (url)=>{
+      const filter = url.split('?')[1].split('=');
+      expect(filter[1]).toBe('mpb')
+    });
+
+    then("o sistema retorna um JSON com o corpo", (responseTest) => {
+      let playlists = mockGetPlaylist();
+      playlists = playlists.filter((playlist:PlaylistEntity) => playlist.genre === 'mpb')
+      let response = JSON.parse(responseTest);
+      expect(playlists).toMatchObject(response)
+    })
+
+    and(/^é retornado um status "(.*)" OK$/, (responseStatusCode) => {
+      let statusCode: number = 0;
+      if(mockGetPlaylist()) statusCode = 200;
+			expect(+responseStatusCode).toBe(statusCode)
+    })
+  })
+
+  test('Playlists por duração', ({given, when, then, and}) => {
+    given(/^que eu sou um usuário logado no sistema com o id "(.*)"$/, async (id)=>{
+      //Checagem com login
+        mockGetPlaylist.mockReturnValue(mockPlaylists)
+    });
+
+    when(/^uma requisição GET for enviada para "(.*)"$/,
+    async (url)=>{
+      const filter = url.split('?')[1].split('=');
+      expect(filter[0]).toBe('duration');
+      expect(filter[1]).toBe('30');
+    });
+
+    then("o sistema retorna um JSON com o corpo", (responseTest) => {
+      let playlists = mockGetPlaylist();
+      playlists = playlists.filter((playlist:PlaylistEntity) => playlist.duration! < 30)
+      let response = JSON.parse(responseTest);
+      expect(playlists).toMatchObject(response)
+    })
+
+    and(/^é retornado um status "(.*)" OK$/, (responseStatusCode) => {
+      let statusCode: number = 0;
+      if(mockGetPlaylist()) statusCode = 200;
+			expect(+responseStatusCode).toBe(statusCode)
+    })
+  })
+
+  test('Playlists por gênero e duração', ({given, when, then, and}) => {
+    given(/^que eu sou um usuário logado no sistema com o id "(.*)"$/, async (id)=>{
+      //Checagem com login
+        mockGetPlaylist.mockReturnValue(mockPlaylists)
+    });
+
+    when(/^uma requisição GET for enviada para "(.*)"$/,
+    async (url)=>{
+      const filter = url.split('?');
+      const genreFilter = filter[1].split('='); //genre=drill
+      const durationFilter = parseInt(filter[2].split('=')[1], 10); //duration=120
+      expect(genreFilter[1]).toBe('drill');
+      expect(durationFilter).toBe(120);
+    });
+
+    then("o sistema retorna um JSON com o corpo", (responseTest) => {
+      let playlists = mockGetPlaylist();
+      playlists = playlists.filter((playlist:PlaylistEntity) => playlist.duration! <= 120 && playlist.genre === 'drill')
+      let response = JSON.parse(responseTest);
+      expect(playlists).toMatchObject(response)
+    });
+
+    and(/^é retornado um status "(.*)" OK$/, (responseStatusCode) => {
+      let statusCode: number = 0;
+      if(mockGetPlaylist()) statusCode = 200;
+			expect(+responseStatusCode).toBe(statusCode)
+    });
+  })
 })
