32f61a43a84063f751aed31411ef9c07947daed6
celobastos <marceloviniciusbs17@gmail.com>
2023-09-05 23:13:54 -0300

    feat: adicionar metodos para tratar as curtidas das playlists

--- src/entities/playlist.entity.ts
+++ src/entities/playlist.entity.ts
@@ -1,30 +1,19 @@
-import BaseEntity from './base.entity';
-
-type IUser = {
-  name: string;
-};
-
-export default class PlaylistEntity extends BaseEntity {
+export default class PlaylistEntity {
+  id: number;
   name: string;
   genre: string;
   description: string;
-  idUser: number;
-  duration?: number;
-  likes?: IUser[];
-  createdBy?: string;
-  qtdMusicas?: number;
-  id_musica?: number[];
+  ownerId: number;
+  createdAt: Date;
+  updatedAt: Date;
 
   constructor(data: PlaylistEntity) {
-    super(data.id || '');
+    this.id = data.id;
     this.name = data.name;
     this.genre = data.genre;
     this.description = data.description;
-    this.qtdMusicas = data.qtdMusicas;
-    this.idUser = data.idUser;
-    this.duration = data.duration;
-    this.likes = data.likes;
-    this.id_musica = data.id_musica;
-    this.createdBy = data.createdBy;
+    this.ownerId = data.ownerId;
+    this.createdAt = data.createdAt;
+    this.updatedAt = data.updatedAt;
   }
 }
