d29067c027864970fc79cf2559f3d3b0859e0af1
Pedro Fernandes Barreto Costa <fernandespedrobc@gmail.com>
2023-09-08 14:46:55 -0300

    CHORE: Current Song bar

--- frontend/src/components/CurrentSong/styles.ts
+++ frontend/src/components/CurrentSong/styles.ts
@@ -6,7 +6,7 @@ export const Wrapper = styled.div`
   height: 6rem;
   background: #1f2232;
   position: fixed;
-  margin-bottom: 0 !important;
-  margin-left: 0 !important;
-  z-index: 100 !important;
+  bottom: 0;
+  left: 0;
+  z-index: 100;
 `;
