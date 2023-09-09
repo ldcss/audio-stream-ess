f263f7f11b6e19882456dd41af7b91746f8676f5
ldcss <ldcs@cin.ufpe.br>
2023-09-09 20:29:46 -0300

    <feat> add routes of playlists

--- frontend/src/Routing.tsx
+++ frontend/src/Routing.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { SignInPage, BrowseArtists } from './pages';
 import { CurrentSong } from './components';
+import Playlists from './pages/Playlists/Playlists';
+import Playlist from './pages/Playlist/Playlist';
 
 const Routing = () => {
   return (
@@ -8,6 +10,8 @@ const Routing = () => {
       <Routes>
         <Route path='/signup' element={<SignInPage />} />
         <Route path='/test' element={<BrowseArtists />} />
+        <Route path='/user/:id/playlist' element={<Playlists />} /> {/*playlists do usuario*/}
+        <Route path='/user/:id/playlist/:idPlaylist' element={<Playlist />} /> {/*playlist x do usuario*/}
       </Routes>
       <CurrentSong />
     </BrowserRouter>
