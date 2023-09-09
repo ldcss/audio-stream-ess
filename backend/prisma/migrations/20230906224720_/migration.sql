f67572c3200e0c64a60b5f00ad90ba3abc82098a
Pedro Fernandes Barreto Costa <fernandespedrobc@gmail.com>
2023-09-09 18:44:46 -0300

    CHORE: adding type to User table

--- backend/prisma/migrations/20230906224720_/migration.sql
+++ backend/prisma/migrations/20230906224720_/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
+    "type" INTEGER NOT NULL
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
