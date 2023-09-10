eb2c1b6365ca0db1b665fdf3aa405f2430446563
joaopmarinho <jpamm@cin.ufpe.br>
2023-09-10 17:17:23 -0300

    CRUD para adicionar e remover música de playlist criados

--- backend/src/repositories/playlist.repository.ts
+++ backend/src/repositories/playlist.repository.ts
@@ -76,6 +76,43 @@ class PlaylistRepository {
   public async deletePlaylist(id: number): Promise<void> {
     await this.db.playlist.delete({ where: { id: id } });
   }
+
+
+
+  public async addMusicToPlaylist(idPlaylist: number, idMusica: number): Promise<Playlist | null> {
+    const result = await this.db.musicToPlaylist.create({
+      data:{
+        musicId: idMusica,
+        playlistId: idPlaylist,
+        createdAt: new Date()
+      }
+    });
+   
+    if(!result){
+      throw new Error("resultado vazio")
+    }
+    const res = this.db.playlist.findUnique({where: { id:idPlaylist }});
+    if(!res){
+      throw new Error("playlist invalida")
+    }
+
+    return res;
+  }
+
+  public async deleteMusicFromPlaylist(idPlaylist: number, idMusica: number): Promise<Playlist | null> {
+    const result = await this.db.musicToPlaylist.deleteMany({
+      where:{
+        playlistId: idPlaylist,
+        musicId: idMusica
+      }
+    });
+   
+    if(!result){
+      throw new Error("Música não encontrada na playlist.")
+    }
+
+    return null;
+  }
 }
 
 export default PlaylistRepository;
