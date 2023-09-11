b6c7a6990fb33ca6f4c2358fc41c44325f5a507a
endersonMarques <emmf@cin.ufpe.br>
2023-09-11 20:24:37 -0300

    removendo backend/src/entities/playlist.entity.ts do pr

--- backend/src/entities/playlist.entity.ts
+++ backend/src/entities/playlist.entity.ts
@@ -1,19 +1,30 @@
-export default class PlaylistEntity {
-  id: number;
+import BaseEntity from './base.entity';
+
+type IUser = {
+  name: string;
+};
+
+export default class PlaylistEntity extends BaseEntity {
   name: string;
   genre: string;
   description: string;
-  ownerId: number;
-  createdAt: Date;
-  updatedAt: Date;
+  idUser: number;
+  duration?: number;
+  likes?: IUser[];
+  createdBy?: string;
+  qtdMusicas?: number;
+  id_musica?: number[];
 
   constructor(data: PlaylistEntity) {
-    this.id = data.id;
+    super(data.id || '');
     this.name = data.name;
     this.genre = data.genre;
     this.description = data.description;
-    this.ownerId = data.ownerId;
-    this.createdAt = data.createdAt;
-    this.updatedAt = data.updatedAt;
+    this.qtdMusicas = data.qtdMusicas;
+    this.idUser = data.idUser;
+    this.duration = data.duration;
+    this.likes = data.likes;
+    this.id_musica = data.id_musica;
+    this.createdBy = data.createdBy;
   }
 }
