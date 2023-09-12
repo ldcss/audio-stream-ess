4d5a284cc6ab3f2bea09a480550051af619141c3
joaopmarinho <jpamm@cin.ufpe.br>
2023-09-12 16:41:39 -0300

    página de músicas dentro da playlist atualizada

--- frontend/src/pages/Playlist/Playlist.tsx
+++ frontend/src/pages/Playlist/Playlist.tsx
@@ -1,4 +1,4 @@
-import { Box, CircularProgress, Container} from "@mui/material";
+import { Box, CircularProgress, Container, Table } from "@mui/material";
 import Navbar from "../../components/Navbar/Navbar";
 import Sidemenu from "../../components/Sidemenu/Sidemenu";
 import { ContainerPlaylist, ImgDiv } from "./styles";
@@ -12,65 +12,148 @@ import likeIcon from "../../assets/likeIcon.svg"
 import musicIcon from "../../assets/musicIcon.svg"
 import shareIcon from "../../assets/shareIcon.svg"
 import timeIcon from "../../assets/timeIcon.svg"
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import AddCircle from "@mui/icons-material/AddCircle";
+import addCircle from "../../assets/addCircle.svg"
 
 
-function Playlist () {
-  const {idUser, idPlaylist} = useParams();
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
+) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+
+];
+
+function Playlist() {
+  const { idUser, idPlaylist } = useParams();
   console.log(idUser, idPlaylist);
   const [playlist, setPlaylist] = useState<PlaylistDto | null>(null);
   useEffect(() => {
-    async function getPlaylist(id: number, idPlaylist: number){
+    async function getPlaylist(id: number, idPlaylist: number) {
       PlaylistService.getPlaylistFromUser(id, idPlaylist).then((response) => {
         setPlaylist(response.data);
         console.log('deu certo!');
       }).catch((e) => console.log('erro: ' + e));
     }
-    if(idUser && idPlaylist)
+    if (idUser && idPlaylist)
       getPlaylist(+idUser, +idPlaylist);
   }, []);
 
-    return (<Container maxWidth={false} sx={{display: 'flex', flexDirection: 'column', height: '100vh', width: '100vw'}}>
-    <Navbar />
-    <Box style={{display: 'flex', flexDirection: 'row', height: 'calc(100vh - 171px)'}}>
+  return (<div style={{width:'100vw', height:'100%', overflowX:'auto'}}>
+
+    <Box style={{ display: 'flex', flexDirection: 'row', height: '100%' }}>
       <Sidemenu />
       <ContainerPlaylist>
-        {playlist ? (<Box sx={{display: 'flex', flexDirection: 'row', border: '1px solid black'}}>
+        {playlist ? (<Box sx={{ display: 'flex', flexDirection: 'row' }}>
           <ImgDiv />
-          <Box sx={{display: 'flex', flexDirection: 'column', border: '1px solid brown', width: '100%', justifyContent: 'center', alignItems: 'flex-start', marginLeft: '20px !important'}}>
-              <h2>{playlist.name}</h2>
-              <h3>{playlist.description}</h3>
-            <Box sx={{display:'flex', flexDirection: 'row', columnGap: '15px'}}>
-              <img src={creatorIcon} alt='like'/>
+          <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%', justifyContent: 'center', alignItems: 'flex-start', marginLeft: '20px !important' }}>
+            <h2>{playlist.name}</h2>
+            <h3>{playlist.description}</h3>
+            <Box sx={{ display: 'flex', flexDirection: 'row', columnGap: '15px' }}>
+              <img src={creatorIcon} alt='like' />
               <h3>{playlist.owner?.name}</h3>
             </Box>
-            <Box sx={{display:'flex', flexDirection: 'row', columnGap: '15px'}}>
-              <img src={dateIcon} alt='like'/>
+            <Box sx={{ display: 'flex', flexDirection: 'row', columnGap: '15px' }}>
+              <img src={dateIcon} alt='like' />
               <h3>{new Date(playlist.createdAt.toString()).toLocaleString().split(',')[0]}</h3>
             </Box>
-            <Box sx={{display: 'flex', flexDirection: 'row', width: '100%', justifyContent: 'flex-start'}}>
-              <Box flex={1} sx={{border: '0.5px solid red', display:'flex', flexDirection: 'row', columnGap: '15px'}}>
-                <img src={likeIcon} alt='like'/>
+
+            { <Box sx={{ display: 'flex', flexDirection: 'row', width: '95%', justifyContent: 'flex-start', paddingLeft: '100px',
+          paddingBottom: '20px' }}>
+              <Box flex={1} sx={{  display: 'flex', flexDirection: 'row', columnGap: '15px' }}>
+                <img src={likeIcon} alt='like' />
                 <p>Curtidas</p>
               </Box>
-              <Box flex={1} sx={{border: '0.5px solid red', display:'flex', flexDirection: 'row', columnGap: '15px'}}>
-                <img src={musicIcon} alt='music'/>
-                <p>Musicas</p>
+              <Box flex={1} sx={{  display: 'flex', flexDirection: 'row', columnGap: '15px' }}>
+              <img src={musicIcon} alt='music' />
+              <p>Musicas</p>
               </Box>
-              <Box flex={1} sx={{border: '0.5px solid red', display:'flex', flexDirection: 'row', columnGap: '15px'}}>
-                <img src={timeIcon} alt='time'/>
-                <p>Minutos</p>
+              <Box flex={1} sx={{  display: 'flex', flexDirection: 'row', columnGap: '15px' }}>
+              <img src={timeIcon} alt='time' />
+              <p>Minutos</p>
               </Box>
-              <Box flex={1} sx={{border: '0.5px solid red', display:'flex', flexDirection: 'row', columnGap: '15px'}}>
-                <img src={shareIcon} alt='share'/>
-                <p>Compartilhe</p>
+              <Box flex={1} sx={{  display: 'flex', flexDirection: 'row', columnGap: '15px' }}>
+              <img src={shareIcon} alt='share' />
+              <p>Compartilhe</p>
               </Box>
-              <Box flex={1} sx={{backgroundColor: '#FDE8E9', borderRadius: '35px', border: '0.5px solid black'}}></Box>
-            </Box>
+              <Box flex={1} sx={{ backgroundColor: '#FDE8E9', borderRadius: '35px', border: '0.5px solid pink' }}></Box>
+            </Box> }
+
+
+            <TableContainer component={Paper} sx={{backgroundColor: "#BC9EC1", paddingBottom:"100px", width: '100%' }}>
+              <Table sx={{ minWidth: 200, backgroundColor: '#1E1E1E', marginLeft:'20px', width:'97%', borderRadius:'15px'}} aria-label="simple table">
+                <TableHead>
+                  <TableRow sx={{marginLeft:'10px'}}>
+                    <TableCell align="center" sx={{color: 'white'}}>Título</TableCell>
+                    <TableCell align="center" sx={{color: 'white'}}>Artista</TableCell>
+                    <TableCell align="center" sx={{color: 'white'}}>Álbum</TableCell>
+                    <TableCell align="center" sx={{color: 'white'}}>Duração</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow
+                    key={row.name}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row" align="center" sx={{color: 'white'}}>{row.calories}</TableCell>
+                      <TableCell align="center" sx={{color: 'white'}}>{row.fat}</TableCell>
+                      <TableCell align="center" sx={{color: 'white'}}>{row.protein}</TableCell>
+                      <TableCell align="center" sx={{color: 'white'}}>{row.carbs}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+              <div style={{display: 'flex', justifyContent: 'center', alignItems:'center', marginTop:'15px'}}>
+                <button style={{display: 'flex', justifyContent: 'center', alignItems:'center', backgroundColor:'#1E1E1E', width: '15%', height:'100%',  borderRadius:'200px'}}>
+                  <img src={addCircle} alt='like' style={{height:'30px', width:'30px'}}/>   Adicionar música
+                </button>
+              </div>
+            </TableContainer>
+
           </Box>
-        </Box>) : (<CircularProgress sx={{margin: 'auto'}} />)}
+        </Box>) : (<CircularProgress sx={{ margin: 'auto' }} />)}
       </ContainerPlaylist>
     </Box>
-  </Container>)
+  </div>
+  )
 }
 
 export default Playlist;
