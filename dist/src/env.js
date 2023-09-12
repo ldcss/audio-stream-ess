f34c5178307ba48a687007dc4f40ce594570ce45
celobastos <marceloviniciusbs17@gmail.com>
2023-09-11 21:27:00 -0300

    fix: delete dist

--- dist/src/env.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Env {
-}
-exports.default = Env;
-Env.ENV = process.env.ENV || 'DEV';
-Env.PORT = process.env.PORT || 5001;
